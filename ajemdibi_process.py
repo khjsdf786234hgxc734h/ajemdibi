dda40874eff757cd3580d4a301a1de2f0588e100
OO OO <oo@home.org>
2018-08-12 17:37:40 +0000

    wip

--- ajemdibi_process.py
+++ ajemdibi_process.py
@@ -3,11 +3,15 @@ import ajemdibi_process_utility as iii
 import shutil
 import urllib.request
 import os
+from config import db_host, db_database, db_user, db_password
+from sqlalchemy import create_engine, text
+
 
 directory    = os.path.expanduser('~') + '/ajemdibi/'
 
 file_movies  = 'ajemdibi_movies_tsv.gz'
 file_ratings = 'ajemdibi_ratings_tsv.gz'
+file_sql_out = 'ajemdibi_movies_final_out.sql'
 
 url_movies   = 'https://datasets.imdbws.com/title.basics.tsv.gz'
 url_ratings  = 'https://datasets.imdbws.com/title.ratings.tsv.gz'
@@ -20,40 +24,32 @@ dict_ratings     = {}
 dict_out         = {}
 
 
-
+################################################################################
 iii.f_print('--------')
-
 iii.f_print('Movies file download started')
 #f = open(file_name, 'bw')
 with urllib.request.urlopen(url_movies) as response, open(directory + file_movies, 'wb') as f:
     shutil.copyfileobj(response, f)
 #f.close()
 iii.f_print('Movies file download finished')
-
+################################################################################
 iii.f_print('---------')
-
 iii.f_print('Ratings file download started')
 with urllib.request.urlopen(url_ratings) as response, open(directory + file_ratings, 'wb') as f:
     shutil.copyfileobj(response, f)
 iii.f_print('Ratings file download finished')
-
+################################################################################
 iii.f_print('---------')
-
 iii.f_print('Ratings file read started')
 list_ratings = iii.f_read_gzip_file(directory + file_ratings)
 iii.f_print('Ratings file read finished')
-
-
+################################################################################
 iii.f_print('--------')
-
 iii.f_print('Movies file read started')
 list_movies = iii.f_read_gzip_file(directory + file_movies)
 iii.f_print('Movies file read finished')
-
-
+################################################################################
 iii.f_print('--------')
-
-
 iii.f_print('Ratings process started')
 
 for i, line in enumerate(list_ratings):
@@ -69,13 +65,9 @@ for i, line in enumerate(list_ratings):
 
 list_ratings_out = []
 iii.f_print('Ratings process finished')
-
-
+################################################################################
 iii.f_print('--------')
-
-
 iii.f_print('Movies process started')
-
 
 for i, line in enumerate(list_movies):
     movie_id,movie_type,title,original_title,is_adult,start_year,end_year,runtime,genres = '','','','','','','','',''
@@ -94,7 +86,6 @@ for i, line in enumerate(list_movies):
 list_movies_out = []
 
 iii.f_print('Movies process finished')
-
 ################################################################################
 
 for key in dict_ratings:
@@ -106,10 +97,9 @@ for key in dict_ratings:
         pass
 
 ################################################################################
-
-
+iii.f_print('--------')
 iii.f_print('File write started')
-with open(directory + 'ajemdibi_movies_final_out.sql', mode = 'wt', encoding = 'utf_8') as f:
+with open(directory + file_sql_out, mode = 'wt', encoding = 'utf_8') as f:
     f.write('truncate `tb_movie`;' + '\n')
     for key, value in dict_out.items():
 
@@ -124,7 +114,7 @@ with open(directory + 'ajemdibi_movies_final_out.sql', mode = 'wt', encoding = '
         "'" + title_primary_ascii.replace("'", "''")   + "', " +\
         "'" + title_secondary_ascii.replace("'", "''") + "', " +\
         year + ", " +\
-        "'" + genre + "', " +\
+        "'" + genre.replace(",", ", ") + "', " +\
         rating + ", " +\
         vote + ", " +\
         "'countries');"
@@ -134,3 +124,16 @@ with open(directory + 'ajemdibi_movies_final_out.sql', mode = 'wt', encoding = '
         f.write(sql + '\n')
     f.write('commit;' + '\n')
 iii.f_print('File write finished')
+################################################################################
+iii.f_print('--------')
+iii.f_print('Database insert started')
+engine = create_engine('mysql+mysqldb://' + db_user + ':' + db_password + '@' + db_host + '/' + db_database , echo = False)
+conn = engine.connect()
+trans = conn.begin()
+with open(directory + file_sql_out, 'r') as f:
+    for sql_dml in f:
+        conn.execute(text(sql_dml))
+trans.commit()
+conn.close()
+iii.f_print('Database insert finished')
+
